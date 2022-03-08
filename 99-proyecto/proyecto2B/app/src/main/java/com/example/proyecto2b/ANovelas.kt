28f55a702511bc1aff3a5f732174a5e5ee638529
cristhiantohasa <cristhian.tohasa@epn.edu.ec>
2022-03-07 21:45:34 -0500

    Proyecto IIB

--- 99-proyecto/proyecto2B/app/src/main/java/com/example/proyecto2b/ANovelas.kt
+++ 99-proyecto/proyecto2B/app/src/main/java/com/example/proyecto2b/ANovelas.kt
@@ -3,6 +3,10 @@ package com.example.proyecto2b
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
@@ -10,6 +14,8 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class ANovelas : AppCompatActivity() {
+
+    var itemSelected = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -53,6 +59,43 @@ class ANovelas : AppCompatActivity() {
     ) {
         val intent = Intent( this,clase )
         startActivity( intent )
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu( menu, v, menuInfo )
+        var inflater = menuInflater
+        inflater.inflate( R.menu.menu_novelas, menu )
+        val info = menuInfo as AdapterView.AdapterContextMenuInfo
+        val id = info.position
+        itemSelected = id
+    }
+
+    override fun onContextItemSelected( item: MenuItem): Boolean {
+        return when( item.itemId ) {
+            R.id.mf_editar -> {
+                openActivityWithParameters( AEditarNovela::class.java )
+                return true
+            }
+            R.id.mf_eliminar -> {
+                return true
+            }
+            R.id.mf_ver -> {
+                return true
+            }
+            else -> return super.onContextItemSelected( item )
+        }
+    }
+
+    fun openActivityWithParameters(
+        clase: Class<*>
+    ){
+        val intentNovela = Intent(this,clase)
+        intentNovela.putExtra( "posicion", itemSelected + 1)
+        startActivity( intentNovela )
     }
 
 }
